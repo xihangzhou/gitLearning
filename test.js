11748b5c2d9d50d6353e823d9568e0d16f62fdfd
zhouxihang <zhouxihang@fenbi.com>
2020-12-08 15:40:03 +0800

    urgent--1

--- test.js
+++ test.js
@@ -9,14 +9,4 @@ class Math {
     var oldValue = descriptor.value;
   
     descriptor.value = function() {
-      console.log(`Calling ${name} with`, arguments);
-      return oldValue.apply(this, arguments);
-    };
-  
-    return descriptor;
-  }
-  
-  const math = new Math();
-  
-  // passed parameters should get logged now
-  math.add(2, 4);
+      console.log(`Calling ${name} with`, arguments
